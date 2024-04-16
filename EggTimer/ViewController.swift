c3a60e75fa4b32ccc4b5cfba64a46f644e8f2249
slv <xout.online@gmail.com>
2024-04-16 10:50:45 +0300

    Second commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -120,6 +120,7 @@ class ViewController: UIViewController {
                     try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
                     try AVAudioSession.sharedInstance().setActive(true)
 
+                    
                     /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
                     player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
 
